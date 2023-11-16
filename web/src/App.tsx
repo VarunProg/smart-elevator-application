46fe9d1d423fdb01d1c5ad4c03e5ca5a6d9b0f7b
Varun Kumar <varun.kumar@uniberg.com>
2023-11-16 10:35:05 +0100

    fix bug for protected routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,13 +15,13 @@ import "bootstrap/scss/bootstrap.scss";
 const App: FC = () => {
   return (
     <Routes>
-      {/* <Route element={<ProtectedRoute />}> */}
+      <Route element={<ProtectedRoute/>}>
         <Route path={ROUTES.LANDING_PAGE} element={<LandingPage />}>
           <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
           <Route path={`${ROUTES.ELEVATORS}/:state`} element={<Elevators />} />
           <Route path={`${ROUTES.ELEVATOR_DETAILS}/:deviceIdentificationNumber`} element={<ElevatorDetails />} />
         </Route>
-      {/* </Route> */}
+      </Route>
       <Route path="*" element={<NotFound />} />
       <Route path={ROUTES.LOGIN} element={<LoginPage />} />
     </Routes>
--- web/src/routes/ProtectedRoute/ProtectedRoute.tsx
+++ web/src/routes/ProtectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,21 @@
-// ProtectedRoute.tsx
-import React from 'react';
-import { withAuthenticationRequired } from '@auth0/auth0-react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, { FC, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate, Outlet } from "react-router-dom";
 
-export const ProtectedRoute = ({ children }) => {
-  //return <>{withAuthenticationRequired(children, { onRedirecting: () => <div>Loading...</div> })}</>;
+export const ProtectedRoute: FC = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const [isAuthResolved, setAuthResolved] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setAuthResolved(true);
+    }
+  }, [isAuthenticated, isLoading]);
+
+  if (!isAuthResolved) {
+    return <div>Loading...</div>;
+  }
+
+  let auth = { token: isAuthenticated };
+  return auth.token ? <Outlet /> : <Navigate to="/login" />;
 };
-
-
